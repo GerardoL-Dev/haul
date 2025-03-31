1f94c61fc00ab05d0910b77f72185e8be6ac8726
GerardoL-Dev <gerardoleivas@gmail.com>
2025-03-31 15:11:17 -0300

    update firebase

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ function App() {
       <Header />      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/haul" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/trainings" element={<Trainings />} />
