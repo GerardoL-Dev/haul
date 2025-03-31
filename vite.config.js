b12408dc9338349c9eb8a4bbd6f22371162402f2
GerardoL-Dev <gerardoleivas@gmail.com>
2025-03-31 14:58:59 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   base: '/haul/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        notFound: 'public/404.html', // Copia el 404.html al build final
-      },
-    },
-  },
-});
+})
